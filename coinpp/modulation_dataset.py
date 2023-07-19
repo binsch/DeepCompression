fix wrong indexing and add normalization

coinpp/modulation_dataset.py
@@ -1,17 +1,30 @@
 import torch
 from torch.utils.data import Dataset
 
+from bisect import bisect
+
 class ModulationDataset(Dataset):
-    def __init__(self, run_id, filename, device):
+    def __init__(self, run_id, filename, device, normalize=True):
         self.modulations = self.load_modulations(run_id, filename, device)
         self.is_patching_enabled = (type(self.modulations is list))
         if self.is_patching_enabled:
-            self.num_patches, self.latent_dim = self.modulations[0].shape
+            self.latent_dim = self.modulations[0].shape[1]
             self.num_samples = len(self.modulations)
-            self.length = self.num_samples * self.num_patches
+            self.num_patches_per_sample = [x.shape[0] for x in self.modulations]
+            self.sample_starts = [0]
+            for n in self.num_patches_per_sample:
+                self.sample_starts.append(self.sample_starts[-1] + n)
+
+            self.modulations = torch.concatenate(self.modulations)
+            self.length = self.modulations.shape[0]
         else:
             self.num_samples, self.latent_dim = self.modulations.shape
             self.length = self.num_samples
+        
+        self.normalize = normalize
+        if normalize:
+            self.mean = torch.mean(self.modulations, dim=0)
+            self.std = torch.std(self.modulations, dim=0)
 
 
     def load_modulations(self, run_id, filename, device):
@@ -31,15 +44,20 @@ class ModulationDataset(Dataset):
         return modulations
 
 
+    def get_sample_index(self, idx):
+        if self.is_patching_enabled:
+            return bisect(self.sample_starts, idx) -  1
+        else:
+            return idx
+
+
     def __len__(self):
         return self.length
     
 
     def __getitem__(self, idx):
-        if self.is_patching_enabled:
-            sample_idx = idx // (self.num_patches * self.latent_dim)
-            patch_idx = (idx % (self.num_patches * self.latent_dim)) // self.latent_dim
-            return self.modulations[sample_idx][patch_idx]
+        if self.normalize:
+            return (self.modulations[idx] - self.mean) / self.std
         else:
             return self.modulations[idx]
 
@@ -47,8 +65,10 @@ class ModulationDataset(Dataset):
 if __name__ == "__main__":
     run_id = "9p36fasn"
     filename = "modulations_train_3_steps.pt"
-    device = "cpu"
+    device = "cuda"
 
     batch_size = 32
 
     dataset = ModulationDataset(run_id, filename, device)
+    print(dataset[0])
+    print(len(dataset))

coinpp/quantization_train.py
@@ -275,7 +275,7 @@ def main(argv):
     
     device = "cuda" if args.cuda and torch.cuda.is_available() else "cpu"
 
-    dataset = ModulationDataset(run_id, filename, device)
+    dataset = ModulationDataset(run_id, filename, "cpu") # should always be on CPU because it's too big
 
     train_dataloader, test_dataloader = process_datasets(dataset, args.batch_size, 0.2, num_workers=args.num_workers, pin_memory=(device == "cuda"))
 